4647db83971060b37f9f25ea7c2ec5f1aa284580
Pranav K <107774675+PkTheCoda@users.noreply.github.com>
2023-06-18 19:38:38 -0400

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,7 +12,7 @@ export default function Header(props) {
                 <form method="dialog">
                     <h1 className="font--title diaEl">How to Play?</h1>
                     <p className="font--title diaEl">This is a very simple <b>memory game</b>. The objective is simple: You have to click on as many cards as you can WITHOUT clicking on the same card twice. The app will automatically keep track of your current score along with your highscore. Feel free to restart and reset the game whenever!</p>
-                    <p className="diaEl" style={{fontFamily: "Outfit"}}>Made with ❤️ by <a href="https://github.com/PkTheCoda target="_blank">PkTheCoda</a></p>
+                    <p className="diaEl" style={{fontFamily: "Outfit"}}>Made with ❤️ by <a href="https://github.com/PkTheCoda" target="_blank">PkTheCoda</a></p>
                     <button className="dialog--button diaEl">Got It!</button>
                 </form>
             </dialog>
